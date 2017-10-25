df4b5566efcbf61e9b9716d31cf3be36d03ed8c3
Brieuc Lambert <lambert.brieuc@gmail.com>
2017-10-25 02:13:44 +0200

    added parameters support and test order requests

--- binance.rb
+++ binance.rb
@@ -64,32 +64,65 @@ class Binance
   ###### Private Data #######
   ###########################
 
-  def get_account
-    response = get_private '/account?'
+  def get_account(opts={})
+    response = get_private '/account?', opts
   end
 
+  # Mandatory parameters for order : symbol side type timeInForce quantity price
+
+  def test_order(opts={})
+    required_opts = %w{ symbol side type timeInForce quantity price }
+      leftover = required_opts - opts.keys.map(&:to_s)
+      if leftover.length > 0
+        raise ArgumentError.new("Required options, not given. Input must include #{leftover}")
+      end
+    response = post_private '/order/test?', opts
+  end
+
+private
+
+  ###########################
+  ###### Create request #####
+  ###########################
+
   def get_private(method, opts={})
-    data = '&timestamp=' + timestamp
-    url = @base_uri + '/v3' + method + data + "&signature=" + signature(timestamp, @api_secret)
-    header = {"X-MBX-APIKEY": @api_key }
-    r = self.class.get(url, query: opts, headers: header)
+    header = {"X-MBX-APIKEY":@api_key }
+    r = self.class.get(set_request(method, opts), headers: header)
     resp = JSON.parse(r.body)
   end
 
-  def timestamp
-    timestamp = DateTime.now.strftime('%Q').to_s
+  def post_private(method, opts={})
+    header = {"X-MBX-APIKEY":@api_key }
+    r = self.class.post(set_request(method, opts), headers: header)
+    resp = JSON.parse(r.body)
   end
 
-  def signature(timestamp, key)
-  data = '&timestamp=' + timestamp
-  digest = OpenSSL::Digest.new('sha256')
-  signature = OpenSSL::HMAC.hexdigest(digest, key, data)
-  p signature
+  def set_request(method, opts={})
+    options = Array.new
+    unless opts.empty?
+      opts.each do |key, value|
+        options << key + "=" + value
+      end
+      data = options.join('&') + '&timestamp=' + DateTime.now.strftime('%Q').to_s
+    else
+      data = '&timestamp=' + DateTime.now.strftime('%Q').to_s
+    end
+
+    digest = OpenSSL::Digest.new('sha256')
+    signature = OpenSSL::HMAC.hexdigest(digest, @api_secret, data)
+
+    url = @base_uri + '/v3' + method + data + "&signature=" + signature
+    return url
   end
 end
-
 binance = Binance.new(api_key, api_secret)
-p binance.get_account
+# p binance.get_account('recvWindow' => '10000')
+p binance.test_order('symbol' => 'LTCBTC',
+                     'side' => 'BUY',
+                     'type' => 'LIMIT',
+                     'timeInForce' => 'GTC',
+                     'quantity' => '1',
+                     'price' => '0.001')
 
 
 
